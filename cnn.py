0c66583f5e5024aa2611130a2e80a1b78faba463
richardejiang <richardejiang@hotmail.com>
2018-04-06 16:03:28 +0800

    imported train and test data

--- cnn.py
+++ cnn.py
@@ -1,1 +1,51 @@
 import keras
+import numpy as np 
+from keras.models import Sequential, Model 
+from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convolution1D
+from keras.layers.merge import Concatenate
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from gensim.models import Word2Vec
+from wv import loadData, processStr
+
+def obtainData(typeOfData = "train"):
+	fileName = "20_train" if typeOfData == "train" else "20_test"
+	sentences, emojis = loadData(fileName)
+
+	texts = [' '.join(ele) for ele in sentences]
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(texts)
+	sequences = tokenizer.texts_to_sequences(texts)
+	data = pad_sequences(sequences, padding='post', truncating='post')
+
+	labels = to_categorical(np.asarray(emojis))
+
+	return data, labels
+
+if __name__ == "__main__":
+	filterSizes = [3, 4, 5]
+	numOfFilters = 10
+	dropout = 0.5
+	batchSize = 50
+	epochs = 20
+	sequenceLength = 20 # Twitter max length is 140 chars
+	wvModel = Word2Vec.load('vectors.bin')
+	sentencesTrain, emojisTrain = obtainData()
+	dataTrain, labelsTrain = obtainData()
+	dataTest, labelsTest = obtainData("test")
+
+	# len(wvModel.wv.vocab)
+	
+	# texts = [' '.join(ele) for ele in sentencesTrain]
+	# tokenizer = Tokenizer()
+	# tokenizer.fit_on_texts(texts)
+	# sequences = tokenizer.texts_to_sequences(texts)
+	# data = pad_sequences(sequences, padding='post', truncating='post')
+
+	# labels = to_categorical(np.asarray(emojisTrain))
+	# print('Shape of data tensor:', data.shape)
+	# print('Shape of label tensor:', labels.shape)
+	
+
+	pass
--- wv.py
+++ wv.py
@@ -1,21 +1,24 @@
 from gensim.models import Word2Vec
 import string
 
-def loadData():
-	tf = open("20_train", "r")
+def loadData(fileName = "20_train"):
+	tf = open(fileName, "r")
 	sentences = []
+	emojis = []
 	for line in tf:
 		x = line.split("\t")[0]
+		y = line.split("\t")[1]
 		x = processStr(x)
 		sentences.append(x.split())
-	return sentences
+		emojis.append(y)
+	return sentences, emojis
 
 def processStr(inputStr):
 	result = inputStr.replace("@user", "").translate(None, string.punctuation).translate(None, string.digits)
 	return result
 
 if __name__ == "__main__":
-	sentences = loadData()
+	sentences,  = loadData()
 	model = Word2Vec(sentences, min_count = 5, size = 50, workers = 6, sg = 0, iter = 50)
 	model.save('vectors.bin')
 	pass
